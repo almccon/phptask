ae7fed73f935030b5ec3773358aab926220a44e8
Alan McConchie <alan.mcconchie@gmail.com>
2013-03-08 21:45:48 -0800

    Added sql to create table

---
+++ create_table.sql
@@ -0,0 +1,15 @@
+--
+-- Create the exchange rate table
+--
+-- I will assume we're using ISO 4217 currency codes so a 3 character name will suffice.
+--
+-- currate is the value of the currency in USD.
+-- Simply multiply the foreign amount by currate to get the value in USD.
+--
+
+CREATE TABLE exchange_rates 
+  (
+    curname CHAR(3),  
+    currate FLOAT
+  )
+;
